584be1a40afe210168dd26b047e41bf2879b8f35
angelrodgz <angelrodriguez@ucol.mx>
2019-07-11 17:59:26 -0500

    Login & Sign Up

---
+++ src/pages/login/login.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  credentialsForm: FormGroup;
+
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              private formBuilder: FormBuilder) {
+
+    this.credentialsForm = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
